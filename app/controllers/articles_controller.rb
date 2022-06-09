1a1abcf1e452e72417a3dacfddf09594c33bab76
shubharthaksangharsha <shubharthaksangharsha@gmail.com>
2022-06-09 08:15:09 +0000

    Added Visible module, also added status feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -45,6 +45,6 @@ class ArticlesController < ApplicationController
   end
   private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :status)
     end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
 
   validates :commenter, presence: true
   validates :body, presence: true
+
 end
